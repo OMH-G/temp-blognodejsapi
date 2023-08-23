1f9ade11a1029c8fdcea42150d02f58f70c51768
Omkar <omkarhalgi50@gmail.com>
2023-08-23 21:36:10 +0530

    json file added

--- index.js
+++ index.js
@@ -1,66 +1,19 @@
-const express = require('express');
-const multer = require('multer');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require('express')
 
-const app = express();
-const port = 8000;
+const app = express()
+const PORT = 4000
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://OMH-G:MongoOmh@testingcluster.ztxfjgz.mongodb.net/starter', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// Define a schema for the uploaded files
-const fileSchema = new mongoose.Schema({
-    filename: String,
-    originalName: String,
-    filePath: String, // Store the path where the file is saved
-    uploadDate: Date,
-});
-
-const File = mongoose.model('File', fileSchema);
-
-// Configure multer for file uploads
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads'); // Set the folder where files will be saved
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
-
-const upload = multer({ storage });
-app.get('/',(req,res)=>{
-    res.send("Hello world");
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
 })
-// Handle file upload
-app.post('/upload', upload.single('file'), async (req, res) => {
-    if (req.file) {
-        const newFile = new File({
-            filename: req.file.fieldname,
-            originalName: req.file.originalname,
-            filePath: req.file.path,
-            uploadDate: new Date(),
-        });
 
-        try {
-            await newFile.save();
-            res.status(200).send('File uploaded and saved to MongoDB.');
-        } catch (error) {
-            res.status(500).send('Error saving file to MongoDB.');
-        }
-    } else {
-        res.status(400).send('No file uploaded.');
-    }
-});
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
 module.exports = app
